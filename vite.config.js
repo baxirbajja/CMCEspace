e51f12681ab153eb39cd65da171a0115e4960df2
bachirbajja <medbachirbajja@gmail.com>
2025-04-19 15:47:19 +0100

    update for deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: true,
+    chunkSizeWarningLimit: 1600,
+  },
+  server: {
+    port: 3000,
+    strictPort: true,
+    host: true
+  }
 })
